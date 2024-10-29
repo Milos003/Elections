23f44458188bc8df7b0a07b4ffea2dcd9a4f82ad
Milos Dukaric <milos.dukaric@student.tugraz.at>
2024-10-29 15:02:52 +0100

    a1 second attempt

--- a1.c
+++ a1.c
@@ -79,6 +79,7 @@ int main(void) {
         while (1) {
             printf("\nPlease enter an ID for candidate %d! (single letter only)\n > ", i + 1);
             scanf(" %c", &input);
+            while (getchar() != '\n');
 
             if (isalpha(input)) {
                 if (is_unique(input, candidate, i)) {
