a5559e22dcb47df97f0c6f53a6c750479c139bf7
Milos Dukaric <milos.dukaric@student.tugraz.at>
2024-11-01 14:34:20 +0100

    a1

--- a1.c
+++ a1.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <ctype.h>
 
-struct CandidatesIDs {
-    char id;
-    int votes;
+#define NUM_CANDIDATES 5
+#define MAX_RETRIES 3  
+
+struct _CandidatesIDs_ {
+    char id_;
+    int votes_;
 };
 
 
-int is_unique(char id, struct CandidatesIDs candidate[], int count) {
+int is_unique(char id_, struct _CandidatesIDs_ candidate[], int count) {
     for (int i = 0; i < count; i++) {
-        if (candidate[i].id == id) {
+        if (candidate[i].id_ == id_) {
             return 0;
         }
     }
@@ -17,129 +20,113 @@ int is_unique(char id, struct CandidatesIDs candidate[], int count) {
 }
 
 
-void election_results(struct CandidatesIDs candidate[], int count) {
-    int total_votes = 0;
+void election_results(struct _CandidatesIDs_ candidate[], int count) {
+    int total_votes_ = 0;
 
-    
     for (int i = 0; i < count; i++) {
-        total_votes += candidate[i].votes;
+        total_votes_ += candidate[i].votes_;
     }
 
     printf("\nThe results of the election are:\n");
 
-    
     for (int i = 0; i < count; i++) {
-        double percentage = total_votes > 0 ? ((double)candidate[i].votes / total_votes) * 100 : 0;
-        printf(" - Candidate \"%c\": %.2f %% (%d votes).\n", candidate[i].id, percentage, candidate[i].votes);
+        double percentage = total_votes_ > 0 ? ((double)candidate[i].votes_ / total_votes_) * 100 : 0;
+        printf(" - Candidate \"%c\": %.2f %% (%d votes).\n", candidate[i].id_, percentage, candidate[i].votes_);
     }
 }
 
 
-void winners(struct CandidatesIDs candidate[], int count) {
-    int max_votes = 0;
-    int tied_candidates = 0;
+void winners(struct _CandidatesIDs_ candidate[], int count) {
+    int max_votes_ = 0;
+    int tied_candidates_ = 0;
 
-    
     for (int i = 0; i < count; i++) {
-        if (candidate[i].votes > max_votes) {
-            max_votes = candidate[i].votes;
+        if (candidate[i].votes_ > max_votes_) {
+            max_votes_ = candidate[i].votes_;
         }
     }
 
-    
     for (int i = 0; i < count; i++) {
-        if (candidate[i].votes == max_votes) {
-            tied_candidates++;
+        if (candidate[i].votes_ == max_votes_) {
+            tied_candidates_++;
         }
     }
 
     printf("\nPlease congratulate the winner(s) of the election:\n");
 
-   
     for (int i = 0; i < count; i++) {
-        if (candidate[i].votes == max_votes) {
-            printf(" - Candidate \"%c\"!\n", candidate[i].id);
+        if (candidate[i].votes_ == max_votes_) {
+            printf(" - Candidate \"%c\"!\n", candidate[i].id_);
         }
     }
 
-    
-    if (tied_candidates > 1) {
-        printf("\nThat's a %d-way tie!\n", tied_candidates);
+    if (tied_candidates_ > 1) {
+        printf("\nThat's a %d-way tie!\n", tied_candidates_);
     }
 }
 
 int main(void) {
     printf("Welcome to the Election Simulator Program!\n");
 
-    struct CandidatesIDs candidate[5];
-    char input;
+    struct _CandidatesIDs_ candidate[NUM_CANDIDATES];
+    char input_;
 
-   
-    for (int i = 0; i < 5; i++) {
+    
+    for (int i = 0; i < NUM_CANDIDATES; i++) {
         while (1) {
             printf("\nPlease enter an ID for candidate %d! (single letter only)\n > ", i + 1);
-            scanf(" %c", &input);
+            scanf(" %c", &input_);
 
-            
-            if (isalpha(input)) {
-                
-                if (is_unique(input, candidate, i)) {
-                    candidate[i].id = input;
-                    break;
-                } else {
-                    printf("[Error] This ID is already in use!\n");
-                }
-            } else {
+            if (isalpha(input_) && is_unique(input_, candidate, i)) {
+                candidate[i].id_ = input_;
+                break;
+            } else if (!isalpha(input_)) {
                 printf("[Error] This input is out of bounds!\n");
+            } else {
+                printf("[Error] This ID is already in use!\n");
             }
+            while (getchar() != '\n'); 
         }
     }
 
     
     while (1) {
-        for (int i = 0; i < 5; i++) {
-            int votes;
+        for (int i = 0; i < NUM_CANDIDATES; i++) {
+            int votes_;
             while (1) {
-                printf("\nPlease enter the amount of votes for candidate \"%c\"! (1 to 1000)\n > ", candidate[i].id);
-                if (scanf("%d", &votes) != 1) {
-                    printf("[Error] This input is out of bounds!\n");
-                    while (getchar() != '\n');  
-                    continue;
-                }
-
-                
-                if (votes >= 1 && votes <= 1000) {
-                    candidate[i].votes = votes;
+                printf("\nPlease enter the amount of votes for candidate \"%c\"! (1 to 1000)\n > ", candidate[i].id_);
+                if (scanf("%d", &votes_) == 1 && votes_ >= 1 && votes_ <= 1000) {
+                    candidate[i].votes_ = votes_;
                     break;
                 } else {
                     printf("[Error] This input is out of bounds!\n");
+                    while (getchar() != '\n');  
                 }
             }
         }
 
         
-        int confirm;
-        printf("\nAre you sure the input is correct?\n 1. Yes\n 2. No\n > ");
-        if (scanf("%d", &confirm) != 1) {
-            printf("[Error] This input is out of bounds!\n");
-            while (getchar() != '\n');  
-            continue;
+        int confirm_;
+        while (1) {
+            printf("\nAre you sure the input is correct?\n 1. Yes\n 2. No\n > ");
+            if (scanf("%d", &confirm_) == 1 && (confirm_ == 1 || confirm_ == 2)) {
+                break;
+            } else {
+                printf("[Error] This input is out of bounds!\n");
+                while (getchar() != '\n');  
+            }
         }
 
-        if (confirm == 1) {
-            break;
-        } else if (confirm == 2) {
-            continue;
+        if (confirm_ == 1) {
+            break;  
         } else {
-            printf("[Error] This input is out of bounds!\n");
-            while (getchar() != '\n');  
-            continue;
+            continue;  
         }
     }
 
     
-    election_results(candidate, 5);
-    winners(candidate, 5);
+    election_results(candidate, NUM_CANDIDATES);
+    winners(candidate, NUM_CANDIDATES);
 
     printf("\nThank you for using the Election Simulator Program!\n");
 
